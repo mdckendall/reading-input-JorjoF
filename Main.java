6cc6f311ebb8b423aba6eeea5128cfca05cb16f3
Jorge Fuerte <fuerte1206@gmail.com>
2022-09-21 07:22:35 -0400

    Added scanner and print statement to main class

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
     System.out.println("Enter a word:");
-    
+    String inputString = myScanner.nextLine();
     //Your code will return the length of the entered String
+
+    System.out.println(inputString.length());
   }
 }
